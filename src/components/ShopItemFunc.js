fdf34d02c263918144ee3e86c4cec68d339debc3
Ekaterina Iachmeneva <iachmenevaev@mail.ru>
2021-09-23 19:12:31 +0300

    first commit

--- src/components/ShopItemFunc.js
+++ src/components/ShopItemFunc.js
@@ -2,27 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import UserModel from '../models/UserModel';
 
-function ShopItemFunc(props){
-    const {item} = props;
- 
-    return (
-     <div class = "main-content">
-<h2>{item.brand}</h2>
-<h1>{item.title}</h1>
-<h3>{item.description}</h3>
-<div className="description">
-{item.descriptionFull}</div>
-<div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
-  <div class="divider"></div>
-  <div class="purchase-info">
-    <div class="price">{item.currency}{item.price}</div>
-    <button>Добавить в корзину</button>
-</div>
-</div>
-    )
+class ShopItemFunc extends React.Component{
+    constructor(props){
+        super();
     }
+
+    render(){
+        const {item} = this.props;
+        return (<div class = "main-content">
+        <h2>item.brand</h2>
+        <h1>item.title</h1>
+        <h3>item.description</h3>
+        <div className="description">
+        item.descriptionFull</div>
+        <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
+          <div class="divider"></div>
+          <div class="purchase-info">
+            <div class="price">item.currency item.price</div>
+            <button>Добавить в корзину</button>
+        </div>
+        </div>)
+    }
+}
+
 ShopItemFunc.propTypes = {
-    user:PropTypes.instanceOf(UserModel).isRequired
+    item:PropTypes.instanceOf(UserModel).isRequired
 }
 
 export default ShopItemFunc;
